cd2444ba8cca4f80b17b699b2cd122b7890adb10
JetCoder <sergor@gmail.com>
2023-07-28 23:10:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "PHP", "MySQL"];
+    var member = {
+        name: "John",
+        age: 30,
+        skills: skills
+    };
+    console.log("Name: " + member.name);
+    console.log("Age: " + member.age);
+    console.log("Skills: " + member.skills);
+}
